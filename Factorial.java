a038160ed82d0aaecf65f882780e8b9cd24ef2f9
Reese Watson <sherrese.watson@gmail.com>
2019-02-13 13:27:37 -0500

    checkpointing work

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,13 @@ import java.math.BigInteger;
 public class Factorial {
 
     public BigInteger factorialOf(Integer value){
-        return null;
+        int result;
+
+        if(value==1)
+         return 1;
+
+       result = factorialOf(value-1) * value;
+       return result;
     }
 
 }
--- IntegerPrinter.java
+++ IntegerPrinter.java
@@ -4,14 +4,33 @@
 public class IntegerPrinter {
 
     public String printIntegerAsBinary(int value){
-        return null;
+        String str = "";
+      for(int a = 0; a < 8; a++)
+      {
+         if(value % 2 == 1)
+         {
+            value = "1" + str;
+         }
+         if(value % 2 == 0)
+         {
+            str = "0" + str;
+         }
+         value = value / 2;
+      }
+        return str;
     }
 
     public String printIntegerAsOctal(int value){
-        return null;
+        int i = new Integer(20);
+        String octal = Integer.toOctalString(i);
+        System.out.println("Octal value: "+octal);
+    
     }
 
     public String printIntegerAsHexadecimal(int value){
+        int i = new Integer();
+        String hex = Integer.toHexString(i);
+        System.out.println("Hex value is " + hex);
         return null;
     }
 
